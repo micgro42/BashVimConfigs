1bd493c227155c48eaad76e77f31bf764f0a2393
Michael Große <mic.grosse+wikitech@googlemail.com>
2015-06-16 23:45:04 +0200

    Update install.sh to handle and powerline

--- install.sh
+++ install.sh
@@ -4,13 +4,26 @@ IFS='\n\t'
 
 HERE=`pwd`
 
-if [ -h "$HOME/.vimrc" ]; then
+if [[ -h "$HOME/.vimrc" ]]; then
         rm -f "$HOME/.vimrc"
 fi
 
-if [ -e "$HOME/.vimrc" ]; then
+if [[ -e "$HOME/.vimrc" ]]; then
         mv "$HOME/.vimrc" "$HOME/.vimrc_bak"
 fi
 
 ln -s "$HERE/.vimrc" "$HOME/.vimrc"
+
+if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
+        mkdir -p ~/.vim/bundle
+        git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +BundleInstall +qall
+
+# cp -u ~/.vim/bundle/powerline/font/PowerlineSymbols.otf ~/.font
+# mkdir -p ~/.config/fontconfig/conf.d/
+# cp -u ~/.vim/bundle/powerline/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+(~/.vim/bundle/fonts/install.sh)
 
